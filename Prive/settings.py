812c3208468ae41d779fb81adec28fe8d1efe3cd
Franco Díaz <user@fraediaz.com>
2022-11-20 22:03:06 -0300

    --

--- Prive/settings.py
+++ Prive/settings.py
@@ -119,7 +119,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+CSRF_TRUSTED_ORIGINS = ['https://prive-test.azurewebsites.net']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
